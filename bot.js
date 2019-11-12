7cdccce1725042a76e1946422a6e7c20b1354794
QuentinGruber <QuentinGruber@gmail.com>
2019-11-12 11:26:25 +0100

    Make code struct

--- bot.js
+++ bot.js
@@ -1,21 +1,61 @@
-const Discord = require('discord.js')
-const bot = new Discord.Client()
+// init API
+const Discord = require('discord.js');
+const Octokit = require("@octokit/rest");
+
+/*
+      *GITHUB BOT INIT*
+ */
+
+// basic auth
+const octokit = new Octokit({  // "octokit" is our Github bot client
+  auth: {
+    username: "KanbanBotDiscord",
+    password: "jesuisunrobot1",
+    async on2fa() {
+      // example: ask the user
+      return prompt("Two-factor authentication Code:");
+    }
+  }
+ });
+ // TODO : console log auth succeed
+
+ 
+/*
+      *DISCORD BOT INIT*
+ */
+
+ // Create bot instance
+const bot = new Discord.Client();
 
 bot.on('ready', () => {
     console.log(`Logged in as ${bot.user.tag}!`);
   });
   
-  // Watch every message's content on the server
-  bot.on('message', msg => {
-    // If a msg is "ping"
-    if (msg.content === 'ping') {
-      msg.reply('pong');
-    }
-  });
-
   // import "Bot_utils.js"
   var Utils = require('./bot_utils.js');
   // Get bot token from auth.json file
   Token = Utils.GetToken()
 
   bot.login(Token)
+
+
+
+
+
+
+  /* SandBox part */
+
+ // Watch every message's content on the server
+ bot.on('message', msg => {
+  // If a msg is "ping"
+  if (msg.content === 'ping') {
+    msg.reply('pong');
+  }
+});
+
+
+  var ListOfIssues = octokit.issues.listForRepo({
+    owner:"KanbanBotDiscord",
+    repo:"repo_test"
+  })
+   console.log(ListOfIssues);
