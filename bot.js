189bead92acfe05255494ffcca3b7c04d6a1b9a8
Kent1 <gruberquentin67@gmail.com>
2020-03-28 00:24:01 +0100

    Add workdays and workhours Close #37

--- bot.js
+++ bot.js
@@ -22,7 +22,23 @@ Config = GetConfigData()
 D_Utils.Authentication_Discord() // launch discord bot
 
 function MainLoop() {
-    G_Utils.GetError()
+    if (Config.WorkHours.length == 0 && Config.WorkDays.length == 0) // if WorkHours & WorkDays not configured
+    {
+        G_Utils.GetError()
+    }
+    else {
+        var today = new Date(); // get new Date info
+        for (var i = 0; i < Config.WorkDays.length; i++) {
+            if (Config.WorkDays[i] == today.getDay()) { // if it's a workDay
+                for (var j = 0; j < Config.WorkHours.length; j++) {
+                    if (today.getHours() - Config.WorkHours[j][0] >= 0 && today.getHours() - Config.WorkHours[j][0] <= 0) { // if it's a workHour
+                        G_Utils.GetError()
+                        //break;
+                    }
+                }
+            }
+        }
+    }
 }
 
 setInterval(function () { MainLoop() }, Config.RefreshTime * 1000)  // Check & display error every T time
