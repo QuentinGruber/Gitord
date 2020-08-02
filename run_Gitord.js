ac6248e0c729535b9ce4e2ff918eef4464f00e10
Quentin Gruber <quentingruber@gmail.com>
2020-08-02 04:23:43 +0200

    remove js files

--- run_Gitord.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var Gitord_1 = require("./Gitord");
-var bot = new Gitord_1.Gitord("Discord token", "Github token", "test-kanban-bot", "repo_test", "Discord chanel ID");
-bot.welcome_message = false;
-bot.RefreshTime = 5;
-bot.Rules.IssueMinimalBody = 1;
-bot.Rules.PullNeedToFix = false;
-bot.Start();
